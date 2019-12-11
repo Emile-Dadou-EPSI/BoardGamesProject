df85c7b90a28138dcbbe3266e5f6a404d0571f7e
dadou97 <emile.dadou97@gmail.com>
2019-12-11 22:58:22 +0100

    Nouvel Version

--- src/main/java/testfirebase.java
+++ src/main/java/testfirebase.java
@@ -1,18 +1,21 @@
-import com.google.api.client.googleapis.auth.oauth2.GoogleCredential;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
-import com.google.firebase.auth.FirebaseCredential;
+import com.google.firebase.auth.FirebaseCredentials;
+import com.google.firebase.database.*;
 
 import java.io.FileInputStream;
 import java.io.IOException;
 
 public class testfirebase {
     // Use the application default credentials
-    private static void main(String[] args) throws IOException {
-        FileInputStream refreshToken = new FileInputStream("path");
+    public static void main(String[] args) throws IOException {
+        FileInputStream refreshToken = new FileInputStream("C:\\Users\\emile\\IdeaProjects\\BoardGames\\src\\main\\java\\boardgames-6813a-firebase-adminsdk-vvivt-657fd5ca31.json");
 
-        FirebaseOptions options = new FirebaseOptions.Builder().setCredential((FirebaseCredential) GoogleCredential.fromStream(refreshToken)).setDatabaseUrl("https://<BDD_NAME>.firebaseio.com/").build();
+        FirebaseOptions options = new FirebaseOptions.Builder().setCredential(FirebaseCredentials.fromRefreshToken(refreshToken)).build();
 
         FirebaseApp.initializeApp(options);
+
+        FirebaseDatabase db;
+       //db = new FirebaseDatabase('');
     }
 }

31acb75b82a33a507ce5714294f0fcbd5b59ab06
dadou97 <emile.dadou97@gmail.com>
2019-12-12 10:40:38 +0100

    Nouvel Version avec Firestore qui fonctionne

--- src/main/java/testfirebase.java
+++ src/main/java/testfirebase.java
@@ -1,21 +1,36 @@
+import com.google.api.core.ApiFuture;
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.cloud.firestore.Firestore;
+import com.google.cloud.firestore.WriteResult;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
-import com.google.firebase.auth.FirebaseCredentials;
-import com.google.firebase.database.*;
+import com.google.firebase.cloud.FirestoreClient;
 
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.ExecutionException;
+
 
 public class testfirebase {
     // Use the application default credentials
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, ExecutionException, InterruptedException {
         FileInputStream refreshToken = new FileInputStream("C:\\Users\\emile\\IdeaProjects\\BoardGames\\src\\main\\java\\boardgames-6813a-firebase-adminsdk-vvivt-657fd5ca31.json");
 
-        FirebaseOptions options = new FirebaseOptions.Builder().setCredential(FirebaseCredentials.fromRefreshToken(refreshToken)).build();
-
+        GoogleCredentials credentials = GoogleCredentials.fromStream(refreshToken);
+        FirebaseOptions options = new FirebaseOptions.Builder().setCredentials(credentials).build();
         FirebaseApp.initializeApp(options);
 
-        FirebaseDatabase db;
-       //db = new FirebaseDatabase('');
+        Firestore db = FirestoreClient.getFirestore();
+
+        // Add document data with id "test"
+        Map<String, Object> data = new HashMap<>();
+        data.put("Name", "Emile");
+        data.put("LastName", "Dadou");
+        //db.collection("FirstTest").document("Test1").set(data);
+        ApiFuture<WriteResult> res = db.collection("test").document("test1").set(data);
+        System.out.print("Update time : " + res.get().getUpdateTime());
     }
+
 }

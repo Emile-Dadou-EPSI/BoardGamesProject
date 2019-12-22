bb5eba5c54df08f11592ec7938c9e10933cd039e
lionel <lionel.batt@epsi.fr>
2019-12-22 21:43:33 +0100

    Front-end modification Bootstrap CSS added 2

--- src/main/java/testfirebase.java
+++ src/main/java/testfirebase.java
@@ -16,7 +16,7 @@ import java.util.concurrent.ExecutionException;
 public class testfirebase {
     // Use the application default credentials
     public static void main(String[] args) throws IOException, ExecutionException, InterruptedException {
-        FileInputStream refreshToken = new FileInputStream("C:\\Users\\emile\\IdeaProjects\\BoardGames\\src\\main\\java\\boardgames-6813a-firebase-adminsdk-vvivt-657fd5ca31.json");
+        FileInputStream refreshToken = new FileInputStream("/home/lionel/Documents/BoardGames/src/main/java/Token/boardgames-6813a-firebase-adminsdk-vvivt-657fd5ca31.json");
 
         GoogleCredentials credentials = GoogleCredentials.fromStream(refreshToken);
         FirebaseOptions options = new FirebaseOptions.Builder().setCredentials(credentials).build();
@@ -26,8 +26,8 @@ public class testfirebase {
 
         // Add document data with id "test"
         Map<String, Object> data = new HashMap<>();
-        data.put("Name", "Emile");
-        data.put("LastName", "Dadou");
+        data.put("Name", "Gout");
+        data.put("LastName", "Cerise");
         //db.collection("FirstTest").document("Test1").set(data);
         ApiFuture<WriteResult> res = db.collection("test").document("test1").set(data);
         System.out.print("Update time : " + res.get().getUpdateTime());
